7eb960efcce46fdf6299147ba663d3fbd4de38a8
cnugroho11 <cnugroho211@gmail.com>
2023-01-29 00:26:23 +0700

    update config

--- lua/cnugroho/remap.lua
+++ lua/cnugroho/remap.lua
@@ -3,4 +3,13 @@ vim.g.mapleader = " "
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
-vim.keymap.set('n', '<leader>a', '<cmd>CodeActionMenu<cr>', { desc = "Open code action" } )
+
+vim.keymap.set('n', '<leader>la', '<cmd>CodeActionMenu<cr>', { desc = "Open code action" })
+
+
+vim.keymap.set('n', '<leader>lh', '<cmd>lua vim.lsp.buf.hover()<cr>', { desc = "Show lsp hover" })
+vim.keymap.set('n', '<leader>ld', '<cmd>lua vim.lsp.buf.definition()<cr>', { desc = "Go to definition" })
+vim.keymap.set('n', '<leader>lD', '<cmd>lua vim.lsp.buf.declaration()<cr>', { desc = "Go to declaration" })
+vim.keymap.set('n', '<leader>ls', '<cmd>lua vim.lsp.buf.signature_help()<cr>', { desc = "Show signature help" })
+vim.keymap.set('n', '<leader>lr', '<cmd>lua vim.lsp.buf.references()<cr>', { desc = "Show references" })
+vim.keymap.set('n', '<leader>li', '<cmd>lua vim.lsp.buf.implementation()<cr>', { desc = "Show implementation" })
